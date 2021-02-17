1e5a15994d380057d945b5ab9eba64129605c9f4
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2021-02-17 13:07:39 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let output = document.getElementById('output');
+let buttons = document.getElementsByClassName('tool--btn');
+for (let btn of buttons) {
+    btn.addEventListener('click', () => {
+        let cmd = btn.dataset['command'];
+        btn.parentElement.classList.toggle("active");
+        if (cmd === 'createlink') {
+            let url = prompt("Enter the link here: ", "http:\/\/");
+            document.execCommand(cmd, false, url);
+        } else {
+            document.execCommand(cmd, false, null);
+        }
+    })
+}
